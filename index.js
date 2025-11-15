eb2dbe2ad7cda3e90ef1f6499f267e55a58546dc
kat <katwang7749@gmail.com>
2025-11-15 17:30:36 -0500

    ts not working

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express()
 app.get("/stats", async (req, res) => {
     try { 
         const email = req.query.email;
+        const apiKey = `4ecc7208-ef8d-45af-8b6c-ceca0c88dcdb`;
         console.log("recieved email: ", email)
 
-        const response = await fetch(`https://hackatime.hackclub.com/api/v1/users/lookup_email/${email}`);
-            
+        const response = await fetch(`https://hackatime.hackclub.com/api/v1/users/lookup_email/${email}`, {
+            headers: { "Authorization": `Bearer ${apiKey}`} 
+        });
             const text = await response.text(); // read raw text
             console.log("API returned:", text);
 
