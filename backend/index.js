985a7847c99367e2057f95727d573df426cb4c04
kat <katwang7749@gmail.com>
2025-11-15 17:34:27 -0500

    added a frontend

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const app = express()
 
-app.get("/stats", async (req, res) => {
-    try { 
-        const email = req.query.email;
-        const apiKey = `4ecc7208-ef8d-45af-8b6c-ceca0c88dcdb`;
-        console.log("recieved email: ", email)
 
-        const response = await fetch(`https://hackatime.hackclub.com/api/v1/users/lookup_email/${email}`, {
-            headers: { "Authorization": `Bearer ${apiKey}`} 
-        });
+app.get("/stats", async (req, res) => {
+    try { const apiKey = `8aab5fa1-1971-4a31-b913-f95fe8fde5aa`;
+
+        const response = await fetch(`https://hackatime.hackclub.com/api/hackatime/v1/users/current/statusbar/today`, {
+                headers: { "Authorization": `Bearer ${apiKey}`} 
+            });
+            
             const text = await response.text(); // read raw text
-            console.log("API returned:", text);
+            console.log("API returned:", text)
 
         if (!response.ok) {
-            return res.send("error: invalid email!");
-        }
+            return res.status(response.status).send(text);
+        } 
 
         let data;
         
